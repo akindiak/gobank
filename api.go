363ac63f6fd5c1134b72cfa8c14d7a94d40a9fce
Artem Kindiak <artiomkindiak@gmail.com>
2023-08-09 11:10:36 +0300

    added ApiServer

---
+++ api.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type ApiServer struct {
+	listenAddr string
+}
+
+func NewApiServer(listenAddr string) *ApiServer {
+	return &ApiServer{
+		listenAddr: listenAddr,
+	}
+}
+
+func (s *ApiServer) Run() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/accounts", makeHandleFunc(s.handleAccount))
+	router.HandleFunc("/accounts/{id}", makeHandleFunc(s.handleGetAccount))
+
+	log.Println("JSON API Server running on port", s.listenAddr)
+	http.ListenAndServe(s.listenAddr, router)
+}
+
+func (s *ApiServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	if r.Method == "GET" {
+		return s.handleGetAccount(w, r)
+	}
+	if r.Method == "POST" {
+		return s.handleCreateAccount(w, r)
+	}
+	if r.Method == "DELETE" {
+		return s.handleDeleteAccount(w, r)
+	}
+
+	return fmt.Errorf("method not allowed %s", r.Method)
+}
+
+func (s *ApiServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	id := mux.Vars(r)["id"]
+	
+	return WriteJSON(w, http.StatusOK, id)
+}
+
+func (s *ApiServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *ApiServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+type ApiError struct {
+	Error string
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+func makeHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			// handle errors in handle funcs
+			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Add("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/akindiak/gobank
 
 go 1.20
+
+require github.com/gorilla/mux v1.8.0 // indirect
